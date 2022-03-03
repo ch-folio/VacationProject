068e4e489019dc60da662f244436d976aeb92f79
ch-folio <chelhuds@gmail.com>
2022-03-02 18:36:54 -0800

    Made all necessary changes and fixed broken links

---
+++ public/javascripts/main.js
@@ -0,0 +1,172 @@
+let vacayArray = [];
+
+
+// define a constructor to create vacation objects
+let VacationObject = function (pID, pCity, pState, pCountry, pType, pVideo) {
+    this.ID = pID;
+    this.City = pCity;
+    this.State = pState;
+    this.Country = pCountry;
+    this.ID = vacayArray.length +1;
+    this.Type = pType;
+    this.Video = pVideo;
+}
+
+vacayArray.push(new VacationObject("1", "Seattle", "Washington", "USA", "Resort","https://www.earthcam.com/usa/washington/seattle/?cam=seattleskyline"));
+vacayArray.push(new VacationObject("2", "London", "England", "UK", "Museum/historical site","https://www.earthcam.com/world/england/london/abbeyroad/?cam=abbeyroad_uk"));
+vacayArray.push(new VacationObject("3", "Sedona", "Arizona", "USA","Shopping site","https://www.earthcam.com/usa/arizona/sedona/?cam=sedona_gateway"));
+vacayArray.push(new VacationObject("4", "Toronto", "Ontario", "Canada","Undetermined","https://www.earthcam.com/world/canada/toronto/cntower/?cam=cntower1"));
+
+
+let selectedType = "not selected";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    createList();
+
+    // add button events ***********************************************************************************
+   
+    document.getElementById("buttonAdd").addEventListener("click", function () {
+
+        vacayArray.push(new VacationObject(document.getElementById("city").value, document.getElementById("state").value, document.getElementById("country").value, selectedType));
+        document.location.href = "index.html#ListAll";
+
+    });
+        
+        document.getElementById("buttonClear").addEventListener("click", function () {
+            document.getElementById("city").value = "";
+            document.getElementById("state").value = "";
+            document.getElementById("country").value = "";
+        });
+
+        $(document).bind("change", "#select-type", function (event, ui) {
+            selectedType = $('#select-type').val();
+        });
+
+        document.getElementById("buttonSortCity").addEventListener("click", function () {
+            vacayArray.sort(dynamicSort("City")); 
+            createList();
+            document.location.href = "index.html#ListAll";
+
+        });
+
+        document.getElementById("buttonSortState").addEventListener("click", function () {
+            vacayArray.sort(dynamicSort("State"));
+            createList();
+            document.location.href = "index.html#ListAll";
+
+        });
+        
+        document.getElementById("buttonSortCountry").addEventListener("click", function () {
+            vacayArray.sort(dynamicSort("Country"));
+            createList();
+            document.location.href = "index.html#ListAll";
+
+        }); 
+
+
+        //button on details page to view the live EarthCam 
+
+        document.getElementById("eCam").addEventListener("click", function () {
+            window.open(document.getElementById("oneVideo").innerHTML);
+        });
+        // end of add button events ******************************************************************
+
+
+        // page before show code *********************************************************************
+
+        // $(document).on("pagebeforeshow", "#ListAll", function (event) {
+        //     createList();
+        // });
+
+        // let newVacation = new Vacation(document.getElementById("ID").value, document.getElementById("City").value, 
+        //    document.getElementById("State").value, document.getElementById("Country").value,
+        //    document.getElementById("Type").value );
+
+        // $.ajax({
+        //     url : "/AddVacation",
+        //     type: "POST",
+        //     data: JSON.stringify(newVacation),
+        //     contentType: "application/json; charset=utf-8",
+        //      success: function (result) {
+        //         console.log(result);
+        //     }
+        // });
+
+        
+        
+        // need one for details page to fill in the info based on the passed in ID
+
+    $(document).on("pagebeforeshow", "#details", function (event) {
+        let localID = localStorage.getItem('parm');
+
+        //force the vacation array to be current
+        vacayArray = JSON.parse(localStorage.getItem('vacayArray'));
+
+        document.getElementById("oneCity").innerHTML = "The city is: " + vacayArray[localID - 1].City;
+        document.getElementById("oneState").innerHTML = "The state is: " + vacayArray[localID - 1].State;
+        document.getElementById("oneCountry").innerHTML = "The country is: " + vacayArray[localID - 1].Country;
+        document.getElementById("oneType").innerHTML = "Destination type: " + vacayArray[localID - 1].Type;
+        document.getElementById("oneVideo").innerHTML = vacayArray[localID - 1].Video;
+    });
+    
+    //end of page before show code
+});
+//end of code that must wait until document has loaded event
+
+
+    function createList() {
+        //clear prior data
+        var divVacationList = document.getElementById("divVacationList");
+        while (divVacationList.firstChild) {  //remove any old data so don't get duplicates
+            divVacationList.removeChild(divVacationList.firstChild);
+        };
+
+        var myul = document.createElement('ul');
+
+
+        vacayArray.forEach(function (oneVacay,) {
+            var myLi = document.createElement('li');
+            myLi.classList.add('oneVacay');
+            myLi.setAttribute("data-parm", oneVacay.ID);
+            myLi.innerHTML = oneVacay.ID + ":  " + oneVacay.City + ", " + oneVacay.State + ", " + oneVacay.Country + ",  "  + oneVacay.Type;
+            myul.appendChild(myLi);
+        });
+        divVacationList.appendChild(myul)
+
+        var liList = document.getElementsByClassName("oneVacay");
+        Array.from(liList).forEach(function (element) {
+            element.addEventListener('click', function () {
+                var parm = this.getAttribute("data-parm");
+                localStorage.setItem('parm', parm);
+
+                let stringVacayArray = JSON.stringify(vacayArray);
+                localStorage.setItem('vacayArray', stringVacayArray);
+                document.location.href = "index.html#details";
+            });
+        });
+    };
+        
+    // $.get("/getAllvacayArray", function(data, status){  // AJAX get
+    //     vacayArray = data;  // put the returned server json data into our local array
+
+        function dynamicSort(property) {
+            var sortOrder = 1;
+    
+            if (property[0] === "-") {
+                sortOrder = -1;
+                property.property.substr(1);
+            }
+    
+                return function (a, b) {
+                    if (sortOrder == -1) {
+                        return b[property].localeCompare(a[property]);
+                    } else {
+                        return a[property].localeCompare(b[property]);
+                    }
+                }
+            }
+    
+    
+
+  
---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+let ServervacayArray = [];
+
+// define a constructor to create vacation objects
+let VacationObject = function (pID, pCity, pState, pCountry, pType, pVideo) {
+  this.ID = pID;
+  this.City = pCity;
+  this.State = pState;
+  this.Country = pCountry;
+  this.ID = ServervacayArray.length +1;
+  this.Type = pType;
+  this.Video = pVideo;
+}
+
+// ServervacayArray.push(new VacationObject("x1", "xSeattle", "Washington", "USA", "Resort", "https://www.earthcam.com/usa/washington/seattle/?cam=seattleskyline" ));
+// ServervacayArray.push(new VacationObject("x2", "xLondon", "England", "UK", "Museum/historical site", "https://www.earthcam.com/world/england/london/abbeyroad/?cam=abbeyroad_uk"));
+// ServervacayArray.push(new VacationObject("x3", "xSedona", "Arizona", "USA","Shopping site", "https://www.earthcam.com/usa/arizona/sedona/?cam=sedona_gateway"));
+// ServervacayArray.push(new VacationObject("x4", "xToronto", "Ontario", "Canada","Undetermined", "https://www.earthcam.com/world/canada/toronto/cntower/?cam=cntower1"));
+
+// console.log(ServervacayArray);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ //res.render('index', { title: 'Express' });
+ res.sendFile('index.html');
+});
+
+/* GET all Notes data */
+router.get('/getAllvacayArray', function(req, res) {
+  res.status(200).json(ServervacayArray);
+});
+
+// /* Add one new note */
+// router.post('/AddVacation', function(req, res) {
+//   const newVacation = req.body;  // get the object from the req object sent from browser
+//   console.log(newVacation);
+//   ServervacayArray.push(newVacation);  // add it to our "DB"  (array)
+//   // prepare a reply to the browser
+//   var response = {
+//     status  : 200,
+//     success : 'Updated Successfully'
+//   }
+//   res.end(JSON.stringify(response)); // send reply
+// });
+
+module.exports = router;
+
+
+
